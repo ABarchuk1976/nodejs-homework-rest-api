1b89e4ed45374abc4b5b342c6afc32280411bf37
Barchuk Andrii <abarchuk1976@gmail.com>
2023-03-05 18:56:30 +0200

    commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,18 +1,38 @@
-// const fs = require('fs/promises')
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-const listContacts = async () => {}
+const CONTACTS_PATH = path.join(__dirname, 'contacts.json');
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+	
+	const buffer = await fsPromises.readFile(CONTACTS_PATH,'utf-8');
+	const contacts = JSON.parse(buffer); 
+	return contacts;
+}
+
+const getById = async (contactId) => {
+	const contacts = await listContacts();
+	
+	const [contact] = contacts.filter(item => item.id === contactId);
+
+	return contact ? {contact, status: 200} : {message: "Not found", status: 404};
+}
 
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+	const contacts = await listContacts();
+	contacts.push(body);
+	console.log("contacts: ", contacts);
+
+	await fsPromises.writeFile(CONTACTS_PATH, JSON.stringify(contacts));
+}
 
 const updateContact = async (contactId, body) => {}
 
 module.exports = {
   listContacts,
-  getContactById,
+  getById,
   removeContact,
   addContact,
   updateContact,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,52 @@
 const express = require('express')
+const dataValidator = require('../../helpers/dataValidator');
+const shortid = require('shortid');
 
 const router = express.Router()
 
+const {
+  listContacts,
+  getById,
+  // removeContact,
+  addContact,
+  // updateContact,
+} = require('../../models/contacts')
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	const contacts = await listContacts();
+  res.json({ status: 200, contacts })
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+	const {id} = req.params;
+
+	const data = await getById(id);
+
+	const {status} = data;
+
+	if (status === 200) {
+		const {contact} = data;
+		res.json({status, contact})
+	} 
+	else {
+		const {message} = data;
+		res.json({status, message})
+	}
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+	
+	console.log('req.body: ', req.body);
+
+	if (!dataValidator(req.body)) {
+		res.json({status: 400, message: "missing required name field"});
+		return;
+	}
+	const id = shortid.generate();
+	const body = {id, ...req.body};
+	addContact(body);
+
+  res.json({status: 201, body})
 })
 
 router.delete('/:contactId', async (req, res, next) => {
